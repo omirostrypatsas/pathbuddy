1ee357f92b3d2841fd1ceb23888ba0c6fb1e8ce0
omirostr1 <115593155+omirostr1@users.noreply.github.com>
2023-07-13 13:48:32 +0100

    first

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { globalColors } from './colors';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.loginbox}>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +16,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: globalColors.orange.background.colour,
     alignItems: 'center',
     justifyContent: 'center',
   },
+  loginbox: {
+    flex: 1,
+    backgroundColor: globalColors.maincolors.white.colour,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 324,
+    marginBottom: 0
+  }
 });
---
+++ colors.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalColors = StyleSheet.create({
+
+    orange: {
+        background: {colour: '#FF8743'},
+    },
+
+    maincolors: {
+        white: {colour: 'FFFFFF'}
+    }
+});
