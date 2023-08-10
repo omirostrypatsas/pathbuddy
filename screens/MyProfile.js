1c08347eac31b7c78e74c9e9790124dbda7e4fcf
omirostr1 <115593155+omirostr1@users.noreply.github.com>
2023-08-10 23:59:24 +0100

    profile and edit profile pic modal

--- screens/MyProfile.js
+++ screens/MyProfile.js
@@ -7,6 +7,7 @@ import { TextInput } from 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native'
 import BottomBar from '../components/BottomBar';
 import { Feather, Ionicons } from '@expo/vector-icons';
+import EditProfilePic from '../modals/editprofilepic';
 
 const { width } = Dimensions.get('screen');
 
@@ -15,6 +16,10 @@ export default function MyProfile() {
     const navigation = useNavigation();
     let firstname = user.firstname
     let lastname = user.lastname
+    const [isModalVisible, setModalVisible] = useState(false);
+    const toggleModal = () => {
+        setModalVisible(!isModalVisible);
+      };
 
     return(
 
@@ -32,6 +37,36 @@ export default function MyProfile() {
             </View>
             <View style={styles.profileinfo}>
                 <Image source={user.image} style={styles.image}/>
+                <View style={styles.userinfo}>
+                    <Text style={styles.firstandlastnames}>{firstname +' '+ lastname}</Text>
+                    <View style={styles.uniinfo}>
+                        {user.uni && <Text style={styles.pupiltext}>Pupil at: </Text>}
+                        {user.uni && <Text style={styles.unitext}>@{user.uni}</Text>}
+                    </View>
+                    {user.bio && <Text style={styles.biotext}>{user.bio}</Text>}
+                </View>
+            </View>
+            <View style={styles.friendsinfo}>
+                <TouchableOpacity style={styles.button1}>
+                    <Text style={styles.buttontext}>Buddies</Text>
+                    <Text style={styles.number}>{user.buddies}</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.button2}>
+                    <Text style={styles.buttontext}>Paths</Text>
+                    <Text style={styles.number}>{user.paths}</Text>
+                </TouchableOpacity>
+            </View>
+            <View style={styles.friendsinfo}>
+                <TouchableOpacity style={styles.editprofileuni}>
+                    <Text style={styles.editprofileunitext}>Edit uni or bio</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.editprofilepic} onPress={toggleModal}>
+                <EditProfilePic isVisible={isModalVisible} toggleModal={toggleModal} />
+                    <Text style={styles.editprofilepictext}>Edit profile picture</Text>
+                </TouchableOpacity>
+            </View>
+            <View style={styles.postsbox}>
+                <Text style={styles.poststext}>Posts</Text>
             </View>
             <View style={styles.bottombar}>
                 <BottomBar activeRoute="MyProfile"/>
@@ -78,10 +113,96 @@ const styles = StyleSheet.create ({
     },
     profileinfo: {
         marginTop: 40,
+        marginLeft: 37,
+        flexDirection: 'row'
     },
     image: {
         height: 92,
-        width: 92
+        width: 92,
+        borderColor: globalColors.orange.background.colour,
+        borderWidth: 2,
+        borderRadius: 50
+    },
+    userinfo: {
+        marginLeft: 13,
+    },
+    firstandlastnames: {
+        fontSize: 19,
+        fontWeight: '700',
+    },
+    uniinfo: {
+        flexDirection: 'row', 
+        marginTop: 8
+    },
+    unitext: {
+        fontWeight: '600'
+    },
+    biotext: {
+        fontSize: 13,
+        marginTop: 8
+    },
+    pupiltext: {
+        color: globalColors.grey.profilepupil.colour
+    },
+    friendsinfo: {
+        marginTop: 50,
+        flexDirection: 'row',
+        alignSelf: 'center'
+    },
+    buttontext: {
+        fontSize: 14,
+        color: globalColors.grey.buttontext.colour,
+        textAlign: 'center'
+    },
+    number: {
+        fontSize: 20,
+        fontWeight: '600',
+        alignSelf: 'center'
+    },
+    button1: {
+        width:100,
+    },
+    button2: {
+        width:100,
+        marginLeft: 45
+    },
+    editprofileuni: {
+        backgroundColor: globalColors.orange.background.colour,
+        height: 45,
+        width: 200,
+        borderRadius: 10,
+    },
+    editprofileunitext: {
+        color: globalColors.maincolors.white.colour,
+        fontSize: 14,
+        textAlign: 'center',
+        paddingTop: 10,
+    },
+    editprofilepic: {
+        marginLeft: 15,
+        backgroundColor: globalColors.grey.buttonchangeimage.colour,
+        height: 45,
+        width: 93,
+        borderRadius: 10,
+    },
+    editprofilepictext: {
+        color: globalColors.maincolors.black.colour,
+        textAlign: 'center',
+        paddingTop: 5
+    },
+    postsbox: {
+        marginTop: 30,
+        height: 40,
+        borderBottomColor: globalColors.orange.background.colour,
+        borderColor: globalColors.maincolors.white.color,
+        borderBottomWidth: 2,
+    },
+    poststext: {
+        color: globalColors.orange.background.colour,
+        fontSize: 15,
+        fontWeight: '500',
+        textAlign: 'center',
+        paddingBottom: 11
     }
 })
 
@@ -89,6 +210,8 @@ const user = {
     image: require('../assets/stick_man.jpg'),
     firstname: 'Hossein',
     lastname: 'Darda',
-    uni: '',
-    bio: '',
+    uni: 'geia',
+    bio: 'eimai vlaks',
+    buddies: 50,
+    paths: 7
 }
